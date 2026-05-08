64c39f81b368eb1130ad10667a8ea5365f50b2e5
Jason Barto <jason.p.barto@gmail.com>
2026-05-08 09:48:08 +0100

    reconfigured the backend to be modified at runtime with variables in hcl backend config file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket = "jpbarto-awsvpoc-us-east-1-harness-tf-state"
-    key    = "psm-jpbarto/guestbook/state"
-    region = "us-east-1"
-  }
+  backend "s3" {}
 }
