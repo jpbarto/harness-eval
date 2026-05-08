af89690aef2cedb501d1e3803c48f0599ace9cd2
Jason Barto <jason.p.barto@gmail.com>
2026-05-08 10:52:34 +0100

    removed overwrite is true in the parameter creation so the parameter can be created with the tags

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -3,10 +3,9 @@ provider "aws" {
 }
 
 resource "aws_ssm_parameter" "jpbarto_app_ns_key" {
-  name      = "jpbarto-app-ns-key"
-  type      = "String"
-  value     = "jpbarto-app-ns-value-rev7"
-  overwrite = true
+  name  = "jpbarto-app-ns-key"
+  type  = "String"
+  value = "jpbarto-app-ns-value-rev7"
 
   tags = {
     squad = "jpbarto"
